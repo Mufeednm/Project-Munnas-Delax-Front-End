91f2c2eb3152f1949bee3f48ed40bd288fdd8f14
mufeednm <mufeedmusthafanm@gmail.com>
2025-04-23 17:13:06 +0530

    git hub

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api', // 🌐 Your backend URL
+  baseURL: 'https://project-munnas-delax-backend.onrender.com/api', // 🌐 Your backend URL
   headers: {
     'Content-Type': 'application/json',
   },
